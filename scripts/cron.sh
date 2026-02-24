44202d95b1cfe7eea5b22cc7aa75f47c071f5ccd
taufiq-r <taufiiqurahman@gmail.com>
2026-02-24 20:32:56 +0700

    refactor:refactore(cron.sh): add maintenance cron jobs and logging setup

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -14,6 +14,17 @@ if ! command -v crond &> /dev/null; then
     apk add --no-cache dcron libcap > /dev/null
 fi
 
+#create logs (fix permission)
+
+touch /var/log/moodle-cron-core.log \
+     /var/log/moodle-backup.log \
+     /var/log/moodle-updates.log \
+     /var/log/moodle-updates-version.log\
+     /var/log/moodle-plugin-updates.log
+
+chown www-data:www-data /var/log/moodle-*.log
+chmod 666 /var/log/moodle-*.log
+
 crontab -u www-data -r 2>/dev/null || true
 
 if [ "$ENVIRONMENT" = "development" ]; then
@@ -28,19 +39,23 @@ fi
 (cat <<CRON
 SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 PGSQL_HOST=${PGSQL_HOST}
 PGSQL_USER=${PGSQL_USER}
 PGSQL_DATABASE=${PGSQL_DATABASE}
-PGPASSWORD=${PGPASSWORD}
+PGPASSWORD=$(cat ${MOODLE_DATABASE_PASSWORD_FILE} 2>/dev/null)
 
+#moodle core cron
+* * * * * flock -n /tmp/moodle-core-cron.lock php /var/www/html/admin/cli/cron.php >> /var/log/moodle-cron-core.log 2>&1
 #database backup
-${BACKUP_CRON} opt/scripts/backup.sh scheduled >> /var/log/moodle-backup.log 2>&1
+${BACKUP_CRON} /opt/scripts/backup.sh scheduled >> /var/log/moodle-backup.log 2>&1
 
 #theme/plugin monitor (5min)
-*/5 * * * * opt/scripts/watch-moodle-updates.sh  >> /var/log/moodle-updates.log 2>&1
+* * * * * flock -n /tmp/check-plugin-updates.lock /opt/scripts/check-plugin-updates.sh >> /var/log/moodle-plugin-updates.log 2>&1
 
 # Version check (every hour)
-0 * * * * opt/scripts/check-moodle-version.sh >> /var/log/moodle-updates-version.log 2>&1
+0 * * * * /opt/scripts/check-moodle-version.sh >> /var/log/moodle-updates-version.log 2>&1
+   
 CRON
 ) | crontab -u www-data -
 
