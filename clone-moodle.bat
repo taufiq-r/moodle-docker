5e7afefe4b9964bb7b324ecaa0db5cf95b359d00
taufiqr <taufiiqurahman@gmail.com>
2026-03-04 10:53:14 +0700

    global config autocrlf

--- clone-moodle.bat
+++ clone-moodle.bat
@@ -1,21 +1,13 @@
-@echo off 
-:: turns off command echoing, only output to shown
-
+@echo off
 setlocal
-:: start a local environment variable, so dont affect global environment
 
 set TARGET=src
-:: set a variable called TARGET with the value "moodle"
 
-if exist "%TARGET\" (
+if exist "%TARGET%" (
     echo Folder %TARGET% already exists, skipping clone
 ) else (
+    git config --global core.autocrlf false
     git clone --branch MOODLE_501_STABLE https://github.com/moodle/moodle.git "%TARGET%"
-) 
-:: if folder "moodle" already exist, skip cloning
-:: it folder "moodle" doesn't exist start cloning
-
+)
 
 endlocal
-:: end local environment, cleanup temporary variable
-
